c8caa00839a981cbc5454a83e16f4c00543b75ab
Abolare Roheemah Ayomide <roheemah@Abolares-MacBook-Air.local>
2024-08-24 18:29:41 +0100

    changed wrong file

--- Crowdfund.sol
+++ Crowdfund.sol
@@ -4,98 +4,100 @@ pragma solidity ^0.8.24;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Bank {
-    address public owner;
-
-    struct BankAccount {
-        string acct_name;
-        address acct_address;
-        uint balance;
+contract Crowdfund {
+    address payable public owner;
+    struct Campaign {
+        uint id;
+        string title;
+        string description;
+        address payable benefactor;
+        address owner;
+        uint goal;
+        uint deadline;
+        uint amountRaised;
+        bool ended;
     }
 
-    mapping (address => uint) internal balanceOf;
-    mapping (address => BankAccount) public user_account;
-    BankAccount[] private acct_array;
-    BankAccount private acct = user_account[msg.sender];
+    // mapping (uint => Campaign) public campaign;
+    Campaign[] public all_campaigns;
 
-    event AccountCreated(address sender, string name, BankAccount _acct);
-    event Transfer(address sender, address to, uint amount);
-    event Withdraw(address sender, uint amount);
+    event CampaignCreated(Campaign newcampaign);
+    event DonationReceived(uint amt);
+    event CampaignEnded();
 
-    // set owner to deployer of the contract
-    constructor () {
+    constructor() {
         owner = payable(msg.sender);
     }
-
-    // fallback() external payable { }
-
-    receive() external payable { }
 
     modifier onlyOwner {
         require(msg.sender == owner, "not owner");
         _;
     }
 
-    modifier notZeroAddress(address addy) {
-        require(addy != address(0), "invalid address");
-        _;
+    receive() external payable { }
+
+    // function to create campaign
+    // goal should be greater than 0
+    function createCampaign(
+        string memory _title,
+        string memory _desc,
+        address _benefactor,
+        uint _goal,
+        uint _duration
+    ) external {
+        require(_goal > 0, "goal cant be 0");
+        require(_duration > block.timestamp, "invalid duration");
+        require(_benefactor != address(0), "invalid address");
+        Campaign memory newCampaign = Campaign({
+            id: all_campaigns.length,
+            title: _title,
+            description: _desc,
+            benefactor: payable(_benefactor),
+            owner: msg.sender,
+            goal: _goal,
+            deadline: block.timestamp + _duration,
+            amountRaised: 0,
+            ended: false
+        });
+
+        all_campaigns.push(newCampaign);
+
+        emit CampaignCreated(newCampaign);
     }
 
-    // modifier validAddress(address addy) {
-    //     require(addy == owner, "not owner");
-    //     _;
-    // }
-    
-    // function to create an account
-    function createAccount(string calldata _name) external returns (BankAccount memory account) {
-        // BankAccount memory acct = user_account[msg.sender];
-        require(msg.sender != address(0), "Cant create an account for this address");
-        require(acct.acct_address == address(0), "You already have an account");
-        // creating the account
-        account = BankAccount(_name, msg.sender,0);
-        // add it to account array
-        acct_array.push(account);
-        user_account[msg.sender] = account;
-        emit AccountCreated(msg.sender, _name, account);
+    // function to donate to campaign
+    function donate(uint _id, uint _amount) external payable returns (string memory) {
+        Campaign memory current_campaign = all_campaigns[_id];
+        require(current_campaign.deadline > block.timestamp, "campaign ended");
+        require(_amount > 0, "amount cant be 0");
+        current_campaign.amountRaised += _amount;
+        all_campaigns[_id] = current_campaign;
+        (bool ok, ) = payable(address(this)).call{value: _amount}("");
+        require(ok, "call not successful");
+
+        emit DonationReceived(_amount);
+        return "donation successful";
     }
 
-    // function to deposit
-    function deposit(uint _amt) external {
-        // BankAccount memory acct = user_account[msg.sender];
-        balanceOf[msg.sender] += _amt;
-        acct.balance += _amt;
-        user_account[msg.sender] = BankAccount(acct.acct_name, msg.sender, balanceOf[msg.sender]);
+    // function to end campaign and xfer to benefactor
+    function endCampaign(uint _id) external payable {
+        Campaign memory target_campaign = all_campaigns[_id];
+        require(target_campaign.deadline <= block.timestamp, "campaign still ongoing");
+        (bool ok, ) = target_campaign.benefactor.call{value: target_campaign.goal}("");
+        require(ok, "call not successful");
+        target_campaign.ended = true;
+
+        emit CampaignEnded();
+
+        toOwner(target_campaign);
     }
 
-    // function to transfer to other accounts
-    function transfer(address payable to, uint _amt) external payable returns (string memory){
-        // BankAccount memory acct = user_account[msg.sender];
-        require(acct.balance >= _amt, "insufficient balance");
-        acct.balance -= _amt;
-        balanceOf[msg.sender] -= _amt;
-        user_account[msg.sender] = BankAccount(acct.acct_name, msg.sender, balanceOf[msg.sender]);
-        (bool successful, ) = to.call{value: _amt}("");
-        require(successful, "not successful");
-        emit Transfer(msg.sender, to, _amt);
-        return "successful";
-    }
-
-    // function to withdraw money from your account
-    function withdraw(uint _amt) external returns (string memory) {
-        // BankAccount memory acct = user_account[msg.sender];
-        require(acct.balance >= _amt, "insufficient balance");
-        acct.balance -= _amt;
-        balanceOf[msg.sender] -= _amt;
-        user_account[msg.sender] = BankAccount(acct.acct_name, msg.sender, balanceOf[msg.sender]);
-        // (bool successful, ) = msg.sender.call{value: _amt}("");
-        // require(successful, "not successful");
-        emit Withdraw(msg.sender, _amt);
-        return "successful";
-    }
-
-    // function to check balance
-    function getBalance() external view returns (uint balance) {
-        // BankAccount memory acct = user_account[msg.sender];
-        balance = acct.balance;
+    // function to transfer remaining amoun to owner
+    function toOwner(Campaign memory _campaign) public payable onlyOwner {
+        if (_campaign.ended && _campaign.amountRaised > _campaign.goal) {
+            uint deficit = _campaign.amountRaised - _campaign.goal;
+            (bool ok, ) = owner.call{value: deficit}("");
+            require(ok, "call not successful");
+        }
     }
 }
